526a29e2835672917c9cac5653429b22331e462f
Charles Thornton <hawthornepresscom@gmail.com>
2019-01-02 22:36:07 -0600

    Initial Release to initial framework for an ES6 version of js_test

---
+++ script.js
@@ -0,0 +1,15 @@
+// Controller
+    
+let Controller = (function() {
+ 
+     
+return {
+    init: function() {
+        console.log('Application has started.');
+    }
+};
+   
+// Other controllers specified within the '( )' below:   
+})();
+
+Controller.init();
